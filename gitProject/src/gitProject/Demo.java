95ea42629ced6e8e7bc4245a49264ac1479cd63f
grhambarde46 <grhambarde46@gmail.com>
2022-11-14 16:40:21 +0530

    My first project

---
+++ gitProject/src/gitProject/Demo.java
@@ -0,0 +1,16 @@
+package gitProject;
+
+import java.util.TreeSet;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		TreeSet<Integer> treeset = new TreeSet<>();
+		treeset.add(10);
+		treeset.add(12);
+		treeset.add(8);
+		treeset.add(5);
+      System.out.println(treeset);
+	}
+
+}
